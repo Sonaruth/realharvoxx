73c900a2d8cab1faeade120486f9e8f2aa8690b1
sonaruth <sonaruth20@gmail.com>
2024-05-16 17:29:55 +0100

    hi

---
+++ index.js
@@ -0,0 +1,135 @@
+// switch statemmment for time
+// let day = "monday";
+// switch (day) {
+//   case "monday":
+//     console.log("it is 5am");
+//     break;
+//   case "tuesday":
+//     console.log("it is 3:30am");
+//     break;
+// }
+
+// if statement
+// let A = "even"
+// let B= "odd"
+// let C = "prime "
+// if(A>B && A>C){
+//     console.log("it is an even number");
+//      if(B>A && B>C){
+//         console.log("it is an odd number");
+//     }
+//     else {
+//         console.log("it is a prime number");
+//     }
+// }
+
+// function addNumbers(x=3,y){
+//     return x+y;
+// }
+// let result=addNumbers(3,5);
+// console.log(result);
+
+// function expression
+// let sum = function(num1,num2)
+// {
+//     return num1 + num2;
+// }
+// let result = sum(5,6)
+// console.log(result);
+
+// if else statement if a particular gender is at home
+// let Emma = "boy"
+// let Priscal = "girl"
+// let result = Priscal > Emma;
+// if(result)
+// console.log("Emma is older.","", "Thank you!");
+// console.log("Thank you!");
+// if elsew
+
+// console.log("Priscal is younger");
+
+// switch statement that prints out the month of the year
+// let monthNumber = 7;
+// switch (monthNumber) {
+//   case 1:
+//       console.log("january");
+//       break;
+//   case 2:
+//       console.log("February");
+//       break;
+//   case 3:
+//       console.log("March");
+//       break;
+//   case 4:
+//       console.log("May");
+//       break;
+//   case 5:
+//       console.log("June");
+//       break;
+//     console.log("July");
+//     break;
+//   case 6:
+//       console.log("August");
+//     break;
+//   case 7:
+//       console.log("september");
+//     break;
+// }
+// const person = { name: 'John', age: 30 };
+// person.age = 31;
+// console.log(person)
+
+// Pre-increment
+// let i = 5;
+// let result = ++i; 
+// console.log(result)
+
+// post increment
+// let i = 5;
+// let result = i++; 
+// console.log(result)
+
+// pre decrement
+// let i = 5;
+// let result = --i; 
+// console.log(result)
+
+// post decrement
+// let i = 5;
+// let result = i--; 
+// console.log(result)
+
+// spread operator
+// var arr1=["monday", "tuesday", "wednesday", "thurdays", "friday"];
+// let arr2;
+// (function(){
+//     arr2= arr1;
+//     arr1[0]="sunday"
+// })();
+// console.log(arr2);
+
+
+//   push function
+// var myArray=[["Ruth",  "is"], ["tall"]];
+// myArray.push([ "yeah", "hahaha"]);
+// console.log(...myArray);
+
+// pop function
+// var myArray=[["Ruth",  "is"], ["tall"]];
+// myArray.pop();
+// console.log(...myArray);
+
+// unshift function
+// let animals=["zebra", "lion", "elephant"];
+// animals.unshift("monkey");
+// console.log(animals);
+
+
+// rest operator
+// const sum = (function(){
+//     return function sum(x, y, z) {
+//         const args = [ x, y, z];
+
+
+//     }
+// } ) 
